63aeaceae9828c8932ee2d4baf0a5011374fd0a2
Ben Bussell <benbussell@gmail.com>
2018-09-12 10:54:25 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var dieRoll1 = Math.floor( Math.random() * 6) + 1;
+var dieRoll2 = Math.floor( Math.random() * 6) + 1;
+var dieRoll3 = Math.floor( Math.random() * 6) + 1;
+alert("You rolled a " + dieRoll1 + " " + dieRoll2 + " " + dieRoll3 + "!");
